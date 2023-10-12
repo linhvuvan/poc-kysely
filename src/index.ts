529c1ea2eeb1a9f1e65de570e782d3e15c87fb34
Linh Vu Van <linhvuvan2022@gmail.com>
2023-10-12 15:37:17 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { Kysely, PostgresDialect, Generated, QueryCreator } from 'kysely';
+import { Pool } from 'pg';
+
+type BookTable = {
+  id: Generated<number>;
+  title: string;
+  author: string;
+};
+
+type Database = {
+  book: BookTable;
+};
+
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    database: 'postgres',
+    host: 'localhost',
+    user: 'postgres',
+    password: 'postgres',
+    port: 5001,
+    max: 10,
+  }),
+});
+
+const db = new Kysely<Database>({
+  dialect,
+});
+
+(async () => {
+  await db.deleteFrom('book').execute();
+
+  await db
+    .insertInto('book')
+    .values({ title: 'Moby Dick', author: 'Herman' })
+    .execute();
+
+  const books = await db
+    .selectFrom('book')
+    .selectAll()
+    .where('title', '=', 'Moby Dick')
+    .execute();
+
+  console.log('books', books);
+})();
